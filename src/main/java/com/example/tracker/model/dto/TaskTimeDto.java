d9589958f31f85e2f02a520b993f1c274cfbab8e
yfedyna <fedynayaroslav@gmail.com>
2023-08-17 16:54:39 +0300

    create TaskTimeDto

---
+++ src/main/java/com/example/tracker/model/dto/TaskTimeDto.java
@@ -0,0 +1,16 @@
+package com.example.tracker.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskTimeDto {
+
+    private Long id;
+    private String timeSpent;
+}
